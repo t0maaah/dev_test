4c272065e12304dc2db7c019a970ca50a3839563
t0maaah <vasseurthomas@laposte.net>
2018-04-25 12:13:04 +0200

    Ajout de la multiplication

--- src/test/Main.java
+++ src/test/Main.java
@@ -3,14 +3,14 @@ package test;
 public class Main {
 
 	public static void main(String[] args) {
-		int nb1 = 10, nb2 = 5, resulat1, resulat2;
+		int nb1 = 10, nb2 = 5, resulat1, resulat2, resulat3;
 		
 		resulat1 = nb1 + nb2;
 		resulat2 = nb1 - nb2;
+		resulat3 = nb1 * nb2;
 		
 		System.out.println(nb1 + " + " + nb2 + " = " + resulat1);
 		System.out.println(nb1 + " - " + nb2 + " = " + resulat2);
-		
+		System.out.println(nb1 + " x " + nb2 + " = " + resulat3);
 	}
-
 }

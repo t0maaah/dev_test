dabb4e1968c9278d79675e88f8d71024ff2e209a
t0maaah <vasseurthomas@laposte.net>
2018-04-25 11:42:39 +0200

    Mon premier commit

---
+++ src/test/Main.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int nb1 = 10, nb2 = 5, resulat;
+		
+		resulat = nb1 + nb2;
+		
+		System.out.println(nb1 + " + " + nb2 + " = " + resulat);
+	}
+
+}

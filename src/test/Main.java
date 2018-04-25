42af00762d7e38ab00891c6e3663bf004b322b89
t0maaah <vasseurthomas@laposte.net>
2018-04-25 11:58:03 +0200

    Ajout de la soustraction

--- src/test/Main.java
+++ src/test/Main.java
@@ -3,11 +3,14 @@ package test;
 public class Main {
 
 	public static void main(String[] args) {
-		int nb1 = 10, nb2 = 5, resulat;
+		int nb1 = 10, nb2 = 5, resulat1, resulat2;
 		
-		resulat = nb1 + nb2;
+		resulat1 = nb1 + nb2;
+		resulat2 = nb1 - nb2;
 		
-		System.out.println(nb1 + " + " + nb2 + " = " + resulat);
+		System.out.println(nb1 + " + " + nb2 + " = " + resulat1);
+		System.out.println(nb1 + " - " + nb2 + " = " + resulat2);
+		
 	}
 
 }
